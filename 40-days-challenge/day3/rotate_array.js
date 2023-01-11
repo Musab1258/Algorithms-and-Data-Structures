02d42804ec9b1fddf8b488635d01f7dbe9b3b73c
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2023-01-11 20:39:45 +0100

    Create rotate_array.js

---
+++ 40-days-challenge/day3/rotate_array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+let rotate = function(nums, k) {
+   if (k === 0 || k % nums.length === 0) return nums
+
+    k = k % nums.length
+
+    swap(0, nums.length - k - 1)
+    swap(nums.length - k, nums.length - 1)
+    swap(0, nums.length - 1)
+
+    function swap(i, j){
+        while(i < j){
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            i++
+            j--
+        }
+    }
+
+    return nums   
+};
